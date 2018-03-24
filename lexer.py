0d8a0b960ee5b3360baec5786c4d5cf1ae68235e
cjjcastro <cjj.castro3@gmail.com>
2018-03-24 19:37:56 -0300

    Create number regex

--- lexer.py
+++ lexer.py
@@ -4,6 +4,18 @@ import re
 # Classe que representa uma token com campos value e type
 Token = namedtuple('Token', ['value', 'type'])
 
+re_number = r'(?P<NUMBER>\-?\d+(\.\d+)?([eE][+-]\d+)?)'  
+re_operator = r'(?P<OPERATOR>[-+*/])'
+re_space = r'(?P<SPACE>\s+)'
+
+re_list = [
+    re_number,
+    re_operator,
+    re_space
+]
+
+re_full = '|'.join(re_list)
+re_compiled = re.compile(re_full)
 
 def lexer(src: str) -> list:
     """
@@ -12,5 +24,10 @@ def lexer(src: str) -> list:
 
     Em caso de erros, levanta uma exceção do tipo SyntaxError.
     """
+
+    tokens = []
+    last_char = 0
+
+    
 
     return [Token('hello', 'string')]
